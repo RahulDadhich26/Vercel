75d40552e7f91239ec01dafc5bb1c2a4b8f25036
TheGaurdiann <rahuldadhich80263@gmail.com>
2025-06-14 16:20:05 +0530

    V1 added

---
+++ Upload service/README.md
@@ -0,0 +1,14 @@
+
+Vercel Project June 13 by Harkirat Singh
+
+Key insights:
+
+1. Uploading the project files -> { a post request has been made containing the project files to be uploaded to a scalable , highly secure and highly durable data storage services };
+
+2. Building the project 
+3. Request made
+
+
+High Level Design of VERCEL Project 
+
+
---
+++ Upload service/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import simpleGit from "simple-git";
+import { generate } from "./utils";
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+// POSTMAN
+app.post("/deploy", async (req, res) => {
+    const repoUrl = req.body.repoUrl;
+    const id = generate(); // asd12
+    await simpleGit().clone(repoUrl, `output/${id}`);
+
+    res.json({
+        id: id
+    })
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
